edcd02a2b74639acb255e070bcb6c6571c7237fb
Enzo Firmino Campanari <enzofirmino.camp@gmail.com>
2024-01-24 08:04:34 -0300

    feat: add task schedule for update user active

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,27 @@
 import fastify from 'fastify'
 import { authRoutes } from './routes/auth'
+import { scheduleJob } from 'node-schedule'
+import { prisma } from './lib/prisma'
 
 const main = async () => {
     const app = fastify()
     await app.register(authRoutes, {
         prefix: 'auth',
+    })
+
+    scheduleJob('0 0 * * *', () => {
+        prisma.$queryRaw`
+            UPDATE "User"
+            SET user_active = FALSE
+            WHERE user_id IN (
+                SELECT user_id
+                FROM "Statement"
+                WHERE DATE('now', '-30 days') > (
+                    SELECT MAX(sta_date)
+                    FROM "Statement"
+                    WHERE user_id = "User".user_id
+                )
+            );`
     })
 
     app.listen({
